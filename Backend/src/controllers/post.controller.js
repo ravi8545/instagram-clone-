1f820c2e1dc1084bffa8e59f99203ff5e41bb11b
ravi8545 <139250843+ravi8545@users.noreply.github.com>
2026-03-21 11:51:58 +0530

    added like feature on the post

--- Backend/src/controllers/post.controller.js
+++ Backend/src/controllers/post.controller.js
@@ -113,7 +113,20 @@ async function likePostController(req, res) {
 }
 
 async function getFeedController(req, res){
-  const posts = await postModel.find().populate("user")
+  const user = req.user
+  const posts =await Promise.all((await postModel.find().populate("user").lean())
+         .map(async (post)=>{
+          const isLiked = await likeModel.findOne({
+               user:user.username,
+               post:post._id
+          })
+         
+          post.isLiked = Boolean(isLiked)
+
+          return post
+
+         }))
+
 
   res.status(200).json({
     message:"Posts fetched successfully",
